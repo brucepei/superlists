e603006ae6ed6785ede1cac406dde58ce18f9c94
brucepei <brucepei@hotmail.com>
2016-03-05 21:52:44 +0800

    add function test to test user input 1 item

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import time
 import unittest
 
@@ -13,21 +14,35 @@ class NewVistorTest(unittest.TestCase):
         #Alice heard that there is a cool web app used to to-do list
         #she go to the home page
         self.browser.get('http://127.0.0.1:8000')
-        time.sleep(1)
+        
         #she found the title contains 'To-Do'
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
         
+        #she found the inputbox which require her to input a to-do item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        #she input a to-do item
+        inputbox.send_keys('Buy peacock feathers')
+        #she input Enter, then the app prompts her to add a new one
+        inputbox.send_keys(Keys.ENTER)
         
-        #she click the botton to add a to-do item
-        
-        #she click the finished button, and the app prompts her to add a new one
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
         
         #she add another item
         
         #she found a link which linked to to-do list
         
         #she click the list botton, and the app return the to-do items as a list
-
+        time.sleep(3)
         self.fail("Test paused!")
         
 if __name__ == '__main__':
--- lists/views.py
+++ lists/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-    return HttpResponse('<html><head><title>To-Do lists</title><body><h1>lpei, hello!</h1></body></html>')
+    return HttpResponse('''<html>
+                        <head>
+                        <title>To-Do lists</title>
+                        <body>
+                        <h1>To-Do lists</h1>
+                        <input type="textbox" id="id_new_item" placeholder="Enter a to-do item" /> 
+                        </body></html>''')
